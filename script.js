fa17f06cbfb57a49ed2535405e726321b7ab9a88
opticck <opticckua@gmail.com>
2016-07-25 12:06:21 +0300

    hiddenlink

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function myHover() {
     document.getElementsByClassName("header-menu")[0].classList.toggle("responsive");
 };
-/*
+
+$(document).ready(function() {
+    $('.hiddenlink').fadeIn(5000);
+});
